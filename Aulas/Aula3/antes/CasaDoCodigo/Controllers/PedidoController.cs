7682e01c2f2f1dd64863b84849685f2599684268
marcelooliveira <mclricardo@gmail.com>
2018-04-02 15:59:24 -0300

    aula 3 - antes

---
+++ Aulas/Aula3/antes/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,59 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+        private readonly IPedidoRepository pedidoRepository;
+        private readonly IItemPedidoRepository itemPedidoRepository;
+
+        public PedidoController(IProdutoRepository produtoRepository,
+            IPedidoRepository pedidoRepository,
+            IItemPedidoRepository itemPedidoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+            this.pedidoRepository = pedidoRepository;
+            this.itemPedidoRepository = itemPedidoRepository;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View(produtoRepository.GetProdutos());
+        }
+
+        public IActionResult Carrinho(string codigo)
+        {
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                pedidoRepository.AddItem(codigo);
+            }
+
+            Pedido pedido = pedidoRepository.GetPedido();
+            return View(pedido.Itens);
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            Pedido pedido = pedidoRepository.GetPedido();
+            return View(pedido);
+        }
+
+        [HttpPost]
+        public void PostQuantidade([FromBody]ItemPedido input)
+        {
+            itemPedidoRepository.UpdateQuantidade(input);
+        }
+    }
+}
