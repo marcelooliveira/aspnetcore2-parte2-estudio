e348b38cbfbe931402328f36cbe88c4e5e5757ee
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-06 13:16:48 -0300

    855-ASPNETCore20Parte2-video2.1-Chamando Repositório de Itens a Partir do Controller

--- Aulas/Aula2/depois/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula2/depois/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -8,13 +8,18 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            throw new NotImplementedException();
         }
     }
 }
