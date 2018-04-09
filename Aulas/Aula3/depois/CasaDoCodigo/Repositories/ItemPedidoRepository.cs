0e340b97174f948162d6a23e86f19ebb9b05dc0a
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-09 18:58:02 -0300

    855-ASPNETCore20Parte2-video3.3-Refatorando Repositórios

--- Aulas/Aula3/depois/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula3/depois/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -8,7 +8,7 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-        void UpdateQuantidade(ItemPedido itemPedido);
+        ItemPedido GetItemPedido(int itemPedidoId);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
@@ -17,19 +17,12 @@ namespace CasaDoCodigo.Repositories
         {
         }
 
-        public void UpdateQuantidade(ItemPedido itemPedido)
+        public ItemPedido GetItemPedido(int itemPedidoId)
         {
-            var itemPedidoDB =
+            return
             dbSet
-                .Where(ip => ip.Id == itemPedido.Id)
+                .Where(ip => ip.Id == itemPedidoId)
                 .SingleOrDefault();
-
-            if (itemPedidoDB != null)
-            {
-                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
-
-                contexto.SaveChanges();
-            }
         }
     }
 }
