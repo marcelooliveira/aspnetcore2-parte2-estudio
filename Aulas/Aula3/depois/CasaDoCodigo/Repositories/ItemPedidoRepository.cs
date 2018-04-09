3738f3aa85651e2db503e05d1bc3e83fca267c46
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-09 19:58:09 -0300

    855-ASPNETCore20Parte2-video3.5-Atualizando Totais do Carrinho na View com JavaScript

--- Aulas/Aula3/depois/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula3/depois/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -9,6 +9,7 @@ namespace CasaDoCodigo.Repositories
     public interface IItemPedidoRepository
     {
         ItemPedido GetItemPedido(int itemPedidoId);
+        void RemoveItemPedido(int itemPedidoId);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
@@ -23,6 +24,11 @@ namespace CasaDoCodigo.Repositories
             dbSet
                 .Where(ip => ip.Id == itemPedidoId)
                 .SingleOrDefault();
+        }
+
+        public void RemoveItemPedido(int itemPedidoId)
+        {
+            dbSet.Remove(GetItemPedido(itemPedidoId));
         }
     }
 }
