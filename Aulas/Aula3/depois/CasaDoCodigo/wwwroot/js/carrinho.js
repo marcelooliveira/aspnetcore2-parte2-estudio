67627713292a1c67298901508e577008484ab4ab
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-09 19:32:57 -0300

    855-ASPNETCore20Parte2-video3.4-Atualizando o Item na View com JavaScript

--- Aulas/Aula3/depois/CasaDoCodigo/wwwroot/js/carrinho.js
+++ Aulas/Aula3/depois/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -34,12 +34,20 @@
             contentType: 'application/json',
             data: JSON.stringify(data)
         }).done(function (response) {
-            
+            let itemPedido = response.itemPedido;
+            let linhaDoItem = $('[item-id=' + itemPedido.id + ']')
+            linhaDoItem.find('input').val(itemPedido.quantidade);
+            linhaDoItem.find('[subtotal]').html((itemPedido.subtotal).duasCasas());
+            debugger;
         });
     }
 }
 
 var carrinho = new Carrinho();
+
+Number.prototype.duasCasas = function () {
+    return this.toFixed(2).replace('.', ',');
+}
 
 
 
