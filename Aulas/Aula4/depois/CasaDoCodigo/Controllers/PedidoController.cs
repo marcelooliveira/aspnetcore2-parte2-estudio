dedda192a2b63c0bf6ecc5fc6460f2279d3cfaeb
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-10 19:16:04 -0300

    855-ASPNETCore20Parte2-video4.5-Validando Dados do Formulário no Servidor

--- Aulas/Aula4/depois/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula4/depois/CasaDoCodigo/Controllers/PedidoController.cs
@@ -56,7 +56,11 @@ namespace CasaDoCodigo.Controllers
         [HttpPost]
         public IActionResult Resumo(Cadastro cadastro)
         {
-            return View(pedidoRepository.GetPedido());
+            if (ModelState.IsValid)
+            {
+                return View(pedidoRepository.UpdateCadastro(cadastro));
+            }
+            return RedirectToAction("Cadastro");
         }
 
         [HttpPost]
